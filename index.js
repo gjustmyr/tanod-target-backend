aa2532c24df53828d36feff84fa16a18f3c3e9c4
gjustmyr <gjustmyrd@gmail.com>
2025-11-22 12:59:39 +0800

    a

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const { connectDB, sequelize } = require('./db');
 const authRouter = require('./routes/auth');
